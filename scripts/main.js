2ccafb65c69a83ff455fa90582333433c26d8aca
CheoR <cheo.roman+github@gmail.com>
2021-01-30 20:39:22 -0600

    Refactor hard-coded form into compoent

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 import { getEntries } from "./JournalDataProvider.js";
 import { EntryListComponent } from "./JournalEntryList.js"
+import { JournalFormComponent } from "./JournalFormComponent.js"
 
+
+JournalFormComponent()
 getEntries().then(() => EntryListComponent())
